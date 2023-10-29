fad32132b32cb74c0b97e407fd53122f8bfcc1ef
alandong2001 <ziand2@uci.edu>
2023-10-29 17:16:37 -0400

    Move database from local to cloud, enabling more dynamic manipulation

--- Data/data_api.py
+++ Data/data_api.py
@@ -4,7 +4,6 @@ import json
 import mysql.connector
 from mysql.connector import errorcode
 
-
 class createUserDataset:
     def __init__(self):
         conf = open("conf.json")
@@ -14,6 +13,8 @@ class createUserDataset:
         self.sql_password = conf_data["SQL_PASSWORD"]
         self.sql_host = conf_data["SQL_HOST"]
         self.sql_database = conf_data["SQL_DATABASE"]
+        self.ssl_ca = conf_data["SQL_SSL_CA"]
+
 
     # Return a list of info json of friends
     def get_user_friends(self, username):
@@ -23,18 +24,26 @@ class createUserDataset:
         friends = data["friends"]["user"]
         return friends
 
-    # Save all the users provided into the database without replicates
     def save_users(self, users):
+        config = {
+            'host': self.sql_host,
+            'user': self.sql_username,
+            'password': self.sql_password,
+            'database': self.sql_database,
+            'client_flags': [mysql.connector.ClientFlag.SSL],
+            'ssl_ca': self.ssl_ca
+        }
+
         try:
-            cnx = mysql.connector.connect(user=self.sql_username, password=self.sql_password, host=self.sql_host,
-                                          database=self.sql_database)
+            cnx = mysql.connector.connect(**config)
         except mysql.connector.Error as err:
             if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                print("Something is wrong with your user name or password")
+                print("Something is wrong with the user name or password")
             elif err.errno == errorcode.ER_BAD_DB_ERROR:
                 print("Database does not exist")
             else:
                 print(err)
+            return
 
         cnx_cursor = cnx.cursor()
 
@@ -52,6 +61,7 @@ class createUserDataset:
 
         cnx_cursor.executemany(sql_save, user_list)
         cnx.commit()
+        cnx.close()
 
         return
 
@@ -90,15 +100,4 @@ def api_get_user_feature(user_name):
 
 
 
-def new_user_feature(conn, user_id, user_name):
-    """
-    Input:
-        - user_id: The ID of the user.
-        - user_name: The name of the user.
-    Output:
-        - From API: dict{'Recent tracks': Recent tracks, 'Top tracks': Top tracks, 'Top artists':Top artists}
-            1. Recent tracks: Dict {key: track_name, value: list[timestamp]}
-            2. Top tracks: Dict {key: track_name, value: count}
-            3. Top artists: Dict {key: artist_name, value: count}
-    """
-    raise NotImplementedError
+
