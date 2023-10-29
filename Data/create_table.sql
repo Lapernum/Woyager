536a2d30e41b0a5d2008b4549c5a16722e92206b
Jinghao Miao <jimiao@ucsd.edu>
2023-10-28 22:59:30 -0400

    add create table sql

---
+++ Data/create_table.sql
@@ -0,0 +1,70 @@
+use treemusic;
+
+DROP TABLE IF EXISTS Artist_tag;
+DROP TABLE IF EXISTS Track_tag;
+DROP TABLE IF EXISTS Tags;
+DROP TABLE IF EXISTS Listening_history;
+DROP TABLE IF EXISTS Top_track;
+DROP TABLE IF EXISTS Tracks;
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Artists;
+
+create table Users (
+	user_id BIGINT NOT NULL,
+    user_name VARCHAR(50) NOT NULL,
+    user_url VARCHAR(100) NOT NULL,
+    primary key (user_id)
+);
+
+create table Artists (
+	artist_id BIGINT NOT NULL,
+    artist_name VARCHAR(50) NOT NULL,
+    artist_url VARCHAR(100) NOT NULL,
+    primary key (artist_id)
+);
+
+create table Tracks (
+	track_id BIGINT NOT NULL,
+    track_name VARCHAR(50) NOT NULL,
+    track_url VARCHAR(100) NOT NULL,
+    artist_id BIGINT NOT NULL,
+    primary key (track_id),
+    foreign key (artist_id) references Artists(artist_id)
+);
+
+create table Top_track (
+	user_id BIGINT NOT NULL,
+    track_id BIGINT NOT NULL,
+    listening_count INTEGER NOT NULL DEFAULT 0,
+    foreign key (user_id) references Users(user_id),
+    foreign key (track_id) references Tracks(track_id)
+);
+
+create table Listening_history (
+	user_id BIGINT NOT NULL,
+    track_id BIGINT NOT NULL,
+    listened_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    foreign key (user_id) references Users(user_id),
+    foreign key (track_id) references Tracks(track_id)
+);
+
+create table Tags (
+	tag_id BIGINT NOT NULL,
+    tag_name VARCHAR(50) NOT NULL,
+    tag_url VARCHAR(100) NOT NULL,
+    primary key (tag_id)
+);
+
+create table Track_tag (
+	tag_id BIGINT NOT NULL,
+    track_id BIGINT NOT NULL,
+    foreign key (tag_id) references Tags(tag_id),
+    foreign key (track_id) references Tracks(track_id)
+);
+
+create table Artist_tag (
+	tag_id BIGINT NOT NULL,
+    artist_id BIGINT NOT NULL,
+    foreign key (tag_id) references Tags(tag_id),
+    foreign key (artist_id) references Artists(artist_id)
+);
