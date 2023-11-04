0b7e35270f0afa15357a82855cc69fd5e27a3072
Zeyu Chang <frankchanggod@gmail.com>
2023-11-04 14:13:49 -0400

    Update data_api.py

--- Data/data_api.py
+++ Data/data_api.py
@@ -64,11 +64,7 @@ class createUserDataset:
         cnx.close()
 
         return
-<<<<<<<<< Temporary merge branch 1
-      
-=========
 
->>>>>>>>> Temporary merge branch 2
 def fetch_high_quality_users(conn):
     """
     Fetches high-quality users from the database
