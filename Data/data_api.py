aefc6d5cbb43cc6c9d3a8c230b240cd9c154d9b0
Alex <wrzhao16@gmail.com>
2023-11-10 17:30:53 -0500

    update

--- Data/data_api.py
+++ Data/data_api.py
@@ -4,107 +4,25 @@ import json
 import mysql.connector
 from mysql.connector import errorcode
 
-class createUserDataset:
-    def __init__(self): 
-        conf = open("conf.json")
-        conf_data = json.load(conf)
-        self.api_key = conf_data["API_KEY"]
-        self.sql_username = conf_data["SQL_USERNAME"]
-        self.sql_password = conf_data["SQL_PASSWORD"]
-        self.sql_host = conf_data["SQL_HOST"]
-        self.sql_database = conf_data["SQL_DATABASE"]
-        self.ssl_ca = conf_data["SQL_SSL_CA"]
-
-
-    # Return a list of info json of friends
-    def get_user_friends(self, username):
-        response = requests.get(
-            "https://ws.audioscrobbler.com/2.0/?method=user.getfriends&user= " + username + "&api_key= " + self.api_key + "&format=json")
-        data = response.json()
-        friends = data["friends"]["user"]
-        return friends
-
-    def save_users(self, users):
-        config = {
-            'host': self.sql_host,
-            'user': self.sql_username,
-            'password': self.sql_password,
-            'database': self.sql_database,
-            'client_flags': [mysql.connector.ClientFlag.SSL],
-            'ssl_ca': self.ssl_ca
-        }
-
-        try:
-            cnx = mysql.connector.connect(**config)
-        except mysql.connector.Error as err:
-            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                print("Something is wrong with the user name or password")
-            elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                print("Database does not exist")
-            else:
-                print(err)
-            return
-
-        cnx_cursor = cnx.cursor()
-
-        sql_fetch = "SELECT user_name FROM Users"
-        cnx_cursor.execute(sql_fetch)
-        already_in = cnx_cursor.fetchall()
-        already_ins = [a[0] for a in already_in]
-
-        user_list = []
-        for user in users:
-            if user["name"] not in already_ins:
-                user_list.append((user["name"], user["url"]))
-
-        sql_save = "INSERT IGNORE INTO Users (user_name, user_url) VALUES (%s, %s)"
-
-        cnx_cursor.executemany(sql_save, user_list)
-        cnx.commit()
-        cnx.close()
-
-        return
-      
-def fetch_high_quality_users(conn):
-    """
-    Fetches high-quality users from the database
-    high-quality users are defined as users who have listened to at least 300 recent tracks (can be repetitive)
-    :param conn: database connection
-    :return: list of user ids
-    """
-    raise NotImplementedError
-
-def get_user_feature(conn, user_id):
-    """
-    Input:
-        - user_id: The ID of the user.
-    Output:
-        - From Database: dict{'Recent tracks': Recent tracks, 'Top tracks': Top tracks, 'Top artists':Top artists}
-            1. Recent tracks: Dict {key: track_id, value: list[timestamp]}
-            2. Top tracks: Dict {key: track_id, value: count}
-            3. Top artists: Dict {key: artist_id, value: count}
-    """
-    raise NotImplementedError
-
-def api_get_user_feature(user_name):
-    """
-    Input:
-        - user_name: The name of the user.
-    Output:
-        - From API: dict{'Recent tracks': Recent tracks, 'Top tracks': Top tracks, 'Top artists':Top artists}
-            1. Recent tracks: Dict {key: track_name, value: list[timestamp]}
-            2. Top tracks: Dict {key: track_name, value: count}
-            3. Top artists: Dict {key: artist_name, value: count}
-    """
-    raise NotImplementedError
-
 # Fetches user features from API.
-class api_get_user_feature:
+class lastfm_api:
     def __self__(self):
         conf = open("conf.json")
         conf_data = json.load(conf)
         self.api_key = conf_data["API_KEY"]
         self.base_url = 'http://ws.audioscrobbler.com/2.0/'
+
+    # Return a list of info json of friends
+    def get_user_friends(self, username):
+        url = f'https://ws.audioscrobbler.com/2.0/?method=user.getfriends&user={username}&api_key={self.api_key}&format=json'
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = response.json()
+            friends = data["friends"]["user"]
+            return friends
+        else:
+            return None
 
     def get_recent_tracks(self, username):
         url = f'{self.base_url}?method=user.getRecentTracks&user={username}&api_key={self.api_key}&format=json'
@@ -116,7 +34,7 @@ class api_get_user_feature:
             for track in data['recenttracks']['track']:
                 track_name = track['name']
                 timestamp = track['date']['uts']
-                recent_tracks[track_name] = timestamp
+                recent_tracks.append({'track_name' : track_name, 'listened_at' : timestamp})
             return recent_tracks
         else:
             return None
@@ -129,9 +47,10 @@ class api_get_user_feature:
             data = response.json()
             top_tracks = {}
             for track in data['toptracks']['track']:
+                track_id = track['id']
                 track_name = track['name']
                 count = int(track['playcount'])
-                top_tracks[track_name] = count
+                top_tracks.append({'track_name': track_name, 'track_id': track_id, 'track_listening_count': count})
             return top_tracks
         else:
             return None
@@ -146,7 +65,7 @@ class api_get_user_feature:
             for artist in data['topartists']['artist']:
                 artist_name = artist['name']
                 count = int(artist['playcount'])
-                top_artists[artist_name] = count
+                top_artists.append({'artist_name' : artist_name, 'artist_listening_count' : count})
             return top_artists
         else:
             return None
@@ -161,13 +80,24 @@ class api_get_user_feature:
             for track in data['toptracks']['track']:
                 track_name = track['name']
                 count = int(track['playcount'])
-                top_tracks[track_name] = count
+                top_tracks.append
             return top_tracks
+        else:
+            return None
+
+    def get_track_id(self, track_name, artist_name):
+        url = f'{self.base_url}?method=track.getInfo&artist={artist_name}&track={track_name}&api_key={self.api_key}&format=json'
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = response.json()
+            track_id = data["track"]["mbid"]
+            return track_id
         else:
             return None
         
 # Fetches user features from the database.
-class database_get_user_feature:
+class database_api:
     """
         Input:
             - user_id: The ID of the user.
@@ -200,6 +130,65 @@ class database_get_user_feature:
             ssl_ca=self.ssl_ca
         )
 
+        try:
+            self.cnx = mysql.connector.connect(**self.conn)
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Something is wrong with the user name or password")
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Database does not exist")
+            else:
+                print(err)
+            return
+
+        self.cnx_cursor = self.cnx.cursor()
+
+    def save_users(self, users):
+        sql_fetch = "SELECT user_name FROM Users"
+        self.cnx_cursor.execute(sql_fetch)
+        already_in = self.cnx_cursor.fetchall()
+        already_ins = [a[0] for a in already_in]
+
+        user_list = []
+        for user in users:
+            if user["name"] not in already_ins:
+                user_list.append((user["name"], user["url"]))
+        
+        newly_added_length = len(user_list)
+
+        sql_save = "INSERT IGNORE INTO Users (user_name, user_url) VALUES (%s, %s)"
+
+        self.cnx_cursor.executemany(sql_save, user_list)
+        self.cnx.commit()
+        self.cnx.close()
+
+        return newly_added_length
+    
+    def save_top_tracks(self, username, top_tracks):
+        sql_getuserid = "SELECT user_id FROM Users WHERE user_name = %s"
+        self.cnx_cursor.execute(sql_getuserid, username)
+        user_id = self.cnx_cursor.fetchall()[0]
+
+        sql_fetch = "SELECT track_id FROM Top_track WHERE user_id = %s"
+        self.cnx_cursor.execute(sql_fetch, user_id)
+        already_in = self.cnx_cursor.fetchall()
+        already_ins = [a[0] for a in already_in]
+
+        track_list = []
+        for track in top_tracks:
+            if track["name"] not in already_ins:
+                user_list.append((user["name"], user["url"]))
+        
+        newly_added_length = len(user_list)
+
+        sql_save = "INSERT IGNORE INTO Users (user_name, user_url) VALUES (%s, %s)"
+
+        self.cnx_cursor.executemany(sql_save, user_list)
+        self.cnx.commit()
+        self.cnx.close()
+
+        return newly_added_length
+
     def get_recent_tracks(self, user_id):
         cursor = self.conn.cursor(dictionary=True)
         query = """
@@ -222,7 +211,7 @@ class database_get_user_feature:
     
     def get_top_tracks(self, user_id):
         cursor = self.conn.cursor(dictionary=True)
-        querry = """
+        query = """
         SELECT Tracks.tract_name, top_track.Count
         FROM top_track
         JOIN tracks ON top_track.track_id = Tracks.track_id
@@ -239,14 +228,14 @@ class database_get_user_feature:
     
     def get_top_artist(self, user_id):
         cursor = self.conn.cursor(dictionary=True)
-        querry = """
+        query = """
         SELECT Artist.artist_name, top_artist.Count
         FROM Artist
         JOIN top_artist ON Artist.artist_id = top_artist.artist_id
         WHERE top_artist.user_id = %s
         """
         cursor.execute(query, (user_id,))
-        top_tracks = {}
+        top_artist = {}
         for row in cursor.fetchall():
             artist_name = row['artist_name']
             count = row['Count']
@@ -259,7 +248,7 @@ class database_get_user_feature:
         query = "SELECT tracks.track_name, listening_history.track_id FROM listening_history JOIN tracks ON listening_history.track_id = tracks.track_id"
         cursor.execute(query)
         history_tracks = cursor.fetchall()
-        historyTracks_dict = {track['track_name']: track['track_id'] for track in top_tracks}
+        historyTracks_dict = {track['track_name']: track['track_id'] for track in history_tracks}
         return historyTracks_dict
     
     def GetAllTopTracks(self):
