564c04f9007dfe397f1f614e3a0d8b727ae16a43
alandong2001 <ziand2@uci.edu>
2023-11-20 19:56:48 -0500

    increase similar user algorithm efficiency

--- main.py
+++ main.py
@@ -11,43 +11,43 @@ from flask import render_template
 
 app = Flask(__name__)
 
-#top_tags_df = concatenate_feature_csvs("Top Tags")
-#top_tags_df = top_tags_df.fillna(0)
-#print(1)
 
-#top_artists_df = concatenate_feature_csvs("Top Artists")
-#top_artists_df = top_artists_df.fillna(0)
-#print(2)
 
-#top_tracks_df = concatenate_feature_csvs("Top Tracks")
-#print(2)
-#top_tracks_df = top_tracks_df.fillna(0)
+top_tags_df = concatenate_feature_csvs("Top Tags")
+top_tags_df = top_tags_df.fillna(0)
+print(1)
+
+top_artists_df = concatenate_feature_csvs("Top Artists")
+top_artists_df = top_artists_df.fillna(0)
+print(2)
+
+top_tracks_df = concatenate_feature_csvs("Top Tracks")
+top_tracks_df = top_tracks_df.fillna(0)
 #print(3)
 
-top_tags_df = pd.read_csv('backend/user/user_features/Top Tags_0.csv')
-top_tags_df = top_tags_df.fillna(0)
+# top tags_df = pd.read_csv('backend/user/Top Tags_0.csv')
+# top artists_df = pd.read_csv('backend/user/Top Artists_0.csv')
+# top tracks_df = pd.read_csv('backend/user/Top Tracks_0.csv')
+# top tags_df = top tags_df.fillna(0)
+# top artists_df = top artists_df.fillna(0)
+# top tracks_df = top tracks_df.fillna(0)
 
-top_artists_df = pd.read_csv('backend/user/user_features/Top Artists_0.csv')
-top_artists_df = top_artists_df.fillna(0)
 
-top_tracks_df = pd.read_csv('backend/user/user_features/Top Tracks_0.csv')
-top_tracks_df = top_tracks_df.fillna(0)
 
 
 explored_user = set()
-explored_user.add('miranta8') #start user
-
+explored_user.add(1) #start user
 
 
 def calculate(username):
-    df = calculate_user_distance(username, top_tracks_df, top_artists_df, top_tags_df)
+    df = calculate_user_distance(username, top_tracks_df, top_artists_df, top_tags_df, explored_user)
     #drop row with explored user
-    df = df[~df['username'].isin(explored_user)][0:10]
+
 
     json_data = df.to_json(orient='records')
 
     # append usernames in df to explored user
-    explored_user.update(df['username'].tolist())
+    explored_user.update(df['user_id'].tolist())
     
     return json_data
 
@@ -59,6 +59,13 @@ def index():
 def get_data(username):
     data = calculate(username)  
     return jsonify(data)
+
+
+
+if __name__ == '__main__':
+    app.run()
+
+
 
 
 
